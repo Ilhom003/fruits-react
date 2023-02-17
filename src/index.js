6acd9cfae4b89a5078abec801647c4441d5e1c73
ilhom003 <abdulahatov03gmail.com>
2023-02-17 22:06:51 +0500

    fruits mini project

--- src/index.js
+++ src/index.js
@@ -2,16 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Img1 from './assets/imgs/cherry.png'
+import Img2 from './assets/imgs/apple.png'
+
+const fruits = [
+  { id: 1, img: Img1, name: 'Cherry', },
+  { id: 2, img: Img2, name: 'Apple', },
+  { id: 3, img: Img1, name: 'Cherry', },
+  { id: 4, img: Img2, name: 'Apple', },
+  { id: 5, img: Img1, name: 'Cherry', },
+  { id: 6, img: Img2, name: 'Apple', },
+  { id: 7, img: Img1, name: 'Cherry', },
+  { id: 8, img: Img2, name: 'Apple', },
+  { id: 9, img: Img1, name: 'Cherry', },
+  { id: 10, img: Img2, name: 'Apple', },
+  { id: 11, img: Img1, name: 'Cherry', },
+  { id: 12, img: Img2, name: 'Apple', },
+  { id: 13, img: Img1, name: 'Cherry', },
+  { id: 14, img: Img2, name: 'Apple', },
+  { id: 15, img: Img1, name: 'Cherry', },
+]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='wrapper'>
+      <h1 className='title'>Fruits</h1>
+      <div className='container'>
+        {fruits.map((fruit) => {
+          return (
+            <App key={fruit.id} data={fruit} />
+
+          )
+        })}
+      </div>
+    </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
